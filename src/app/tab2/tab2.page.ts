b1e70573429b121843a0820ac9ed417b8228030a
renaonfire <renate.brook@gmail.com>
2020-04-09 23:23:29 +0100

    update data retrieve func and add array reduce

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,44 +10,17 @@ import { SpendService } from '../services/spend.service';
 })
 export class Tab2Page implements OnInit{
   items: any[];
+  sum: any;
+  result: any;
 
   constructor(private spendService: SpendService) {
   } 
 
-  getData() {
-    // firebase.database().ref('spend').child('May').once('value').then(function(snapshot) {
-    //       let docs = [(snapshot.val())] || [];
-    //       docs.map(a => {
-    //         const object = a
-    //         // console.log(...object);
-    //         // return {...object};
-    //       })
-          // let doc = docs[0]
-          // let object = doc.amount;
-          // console.log(doc);
-          // console.log(object);
-          // this.items = docs;
-          
-    //     }
-    // )
 
-// .snapshotChanges()
-// .map(actions => {
-//    return actions.map(a => {
-//      //Get document data
-//      const data = a.payload.doc.data() as Task;
-//      //Get document id
-//      const id = a.payload.doc.id;
-//      //Use spread operator to add the id to the document data
-//      return { id, …data };
-//    });
-// });
-    // return this.items;
-  }
 
   ngOnInit() {
     
-    this.spendService.searchData();
+    this.spendService.getSumOfSpend();
 
   }
 
